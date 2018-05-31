5aae4db15d1d3c28d6860a0e986c231430e9d9f2
LukyMentor <31289985+LukyMentor@users.noreply.github.com>
2018-05-31 18:21:59 +0200

    Added Timeline and few more things

--- Main/script.js
+++ Main/script.js
@@ -1,15 +1,26 @@
-$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
-    if (!$(this).next().hasClass('show')) {
-      $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
-    }
-    var $subMenu = $(this).next(".dropdown-menu");
-    $subMenu.toggleClass('show');
-  
-  
-    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
-      $('.dropdown-submenu .show').removeClass("show");
-    });
-  
-  
-    return false;
+$('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+  if (!$(this).next().hasClass('show')) {
+    $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+  }
+  var $subMenu = $(this).next(".dropdown-menu");
+  $subMenu.toggleClass('show');
+
+
+  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+    $('.dropdown-submenu .show').removeClass("show");
   });
+
+
+  return false;
+});
+
+function anotation() {
+  if (document.getElementById('usn').value != "" || document.getElementById('pwd').value != "") {
+    if (document.getElementById('usn').value == "admin" || document.getElementById('pwd').value == "admin") {
+      alert("Dobrý pokus!");
+    }
+    else {
+      alert("Zde nic není :(");
+    }
+  }
+}
